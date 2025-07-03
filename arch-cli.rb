c3dfffe4a4eb3941c416f5b1f7f8e2267c1524e1
Brian Hoffman <hoffmabc@users.noreply.github.com>
2025-07-03 09:43:05 -0400

    Update arch-cli.rb

--- arch-cli.rb
+++ arch-cli.rb
@@ -6,19 +6,19 @@ class ArchCli < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/Arch-Network/arch-node/releases/download/v#{version}/cli-aarch64-apple-darwin"
-      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+      url "https://github.com/Arch-Network/arch-node/releases/download/v#{version}/arch-cli-aarch64-apple-darwin"
+      sha256 "b9820c770ca67d6fb8561e5df1a7c68b0ceaf092e9e59900c7e60adf48ac785a"
     end
     on_intel do
-      url "https://github.com/Arch-Network/arch-node/releases/download/v#{version}/cli-x86_64-apple-darwin"
-      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+      url "https://github.com/Arch-Network/arch-node/releases/download/v#{version}/arch-cli-x86_64-apple-darwin"
+      sha256 "59cd1694b1b31fc620035f15dd2e70caa5293bfcd84e9f3b9f160866963cf23e"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/Arch-Network/arch-node/releases/download/v#{version}/cli-x86_64-unknown-linux-gnu"
-      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+      url "https://github.com/Arch-Network/arch-node/releases/download/v#{version}/arch-cli-x86_64-unknown-linux-gnu"
+      sha256 "ee6759f62e0bff92cd2922b8618620d802fb17bbd3b9e503946f73caa5d6cb9d"
     end
   end
 
